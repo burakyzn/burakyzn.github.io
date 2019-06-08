a6d0120e9be2058daa79c05d9e7e51415fde0f46
Burak Yazan <44683436+burakyzn@users.noreply.github.com>
2019-06-08 14:58:14 +0300

    create dicerolller app

---
+++ demo/diceroller/src/app.js
@@ -0,0 +1,56 @@
+const first_down = document.querySelector('#firstDown');
+const second_down = document.querySelector('#secondDown');
+const modal_body = document.querySelector('#modal-body')
+
+$(document).ready(function () {
+    setup();
+});
+
+function setup(){
+    var dice_col_height = $('#dice-col').css('height');
+    var dice_height = (parseFloat(dice_col_height) - 50) + 'px'; 
+    $('#app-dice').css('font-size', dice_height);
+
+    var title_col_height = $('#title-col').css('height');
+    var title_height = (parseFloat(title_col_height) - 20) + 'px';
+    $('#app-title').css('font-size', title_height);
+
+   
+    for(var i = 0; i<10;i++){
+        var newOption = document.createElement('option');
+        newOption.value = i+1;
+        newOption.innerHTML = i+1;
+        second_down.appendChild(newOption);
+    }
+
+    for(var i = 1; i<20;i++){
+        var newOption = document.createElement('option');
+        newOption.value = i+1;
+        newOption.innerHTML = i+1;
+        first_down.appendChild(newOption);
+    }
+}
+
+$('#rollButton').click(function (e) { 
+    var number_of_side = first_down.value;
+    var number_of_dice = second_down.value;
+
+    var result_arr = [];
+    for(var i=0; i < number_of_dice;i++){
+        var random_dice = Math.floor(Math.random() * number_of_side) + 1;
+        result_arr.push(random_dice)
+    }
+
+    var result = result_arr.join('+');
+
+    var total = 0;
+    result_arr.forEach(function(e){
+        total += e;
+    })
+
+    var average = total / result_arr.length;
+
+    modal_body.innerHTML = 'Result : ' + result + '<br>' + 'Total : '+ total + '<br>' + 'Average : ' + average;
+
+    $('#myModal').modal('show');
+});
